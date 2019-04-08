e9118c5dd1a076307e49e4ffb5d8df5e738d7a6b
Davis <davisperchik@gmail.com>
2019-04-08 11:25:40 -0400

    done zipcodewilmington, metting with Pat, then continue

--- src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
@@ -1,22 +1,41 @@
 package rocks.zipcode.io.quiz4.collections;
 
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author leon on 11/12/2018.
  */
 public class ZipCodeWilmington {
+
+    private List<Student> studentList;
+
+    public ZipCodeWilmington(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public ZipCodeWilmington() {
+        studentList = new ArrayList<>();
+    }
+
     public void enroll(Student student) {
+        this.studentList.add(student);
     }
 
     public Boolean isEnrolled(Student student) {
-        return null;
+        return this.studentList.contains(student);
     }
 
     public void lecture(double numberOfHours) {
+        for (Student student : studentList){
+            student.learn(numberOfHours);
+        }
     }
 
     public Map<Student, Double> getStudyMap() {
-        return null;
+        Map<Student, Double> studentMap = new LinkedHashMap<>();
+        for (Student student : studentList){
+            studentMap.put(student, student.getTotalStudyTime());
+        }
+        return studentMap;
     }
 }
