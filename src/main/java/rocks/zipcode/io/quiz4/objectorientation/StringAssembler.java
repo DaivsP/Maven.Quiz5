32dd548826a9bd0f46f50fc3f4592b3069ac3079
Davis <davisperchik@gmail.com>
2019-04-08 10:29:13 -0400

    object orientation done

--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
@@ -4,14 +4,24 @@ package rocks.zipcode.io.quiz4.objectorientation;
  * @author leon on 11/12/2018.
  */
 public class StringAssembler {
+
+    private StringBuilder answer;
+    private Character delimeter;
+
+
     public StringAssembler(Character delimeter) {
+        this.delimeter = delimeter;
+        answer = new StringBuilder();
     }
 
     public StringAssembler append(String str) {
-        return null;
+        answer.append(str);
+        answer.append(delimeter);
+        return this;
     }
 
     public String assemble() {
-        return null;
+        answer.deleteCharAt(answer.length() - 1);
+        return answer.toString();
     }
 }
