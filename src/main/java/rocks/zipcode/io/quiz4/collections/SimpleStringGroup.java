e5188d8ffa35000e559690df3dafe5faa3f1af65
Davis <davisperchik@gmail.com>
2019-04-08 12:33:23 -0400

    done collections/ half way

--- src/main/java/rocks/zipcode/io/quiz4/collections/SimpleStringGroup.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/SimpleStringGroup.java
@@ -1,31 +1,61 @@
 package rocks.zipcode.io.quiz4.collections;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
 /**
  * @author leon on 11/12/2018.
  */
-public class SimpleStringGroup {
+public class SimpleStringGroup implements Iterable{
+
+    Integer count;
+    List<String> group;
+
     public SimpleStringGroup() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        this.group = new ArrayList<>();
     }
 
     public Integer count() {
-        return null;
+        return group.size();
     }
 
     public void insert(String string) {
+        group.add(string);
     }
 
     public Boolean has(String string) {
-        return null;
+        return group.contains(string);
     }
 
     public String fetch(int indexOfValue) {
-        return null;
+        return group.get(indexOfValue);
     }
 
     public void delete(String string) {
+        group.remove(string);
     }
 
     public void clear() {
+        group.clear();
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public void forEach(Consumer action) {
+        for (String string : group){
+            action.accept(string);
+        }
+    }
+
+    @Override
+    public Spliterator spliterator() {
+        return null;
     }
 }
